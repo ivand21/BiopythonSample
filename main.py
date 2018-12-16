8bb5fa29e1b82aa1bb0e86ca296c3855ff6a5c04
Jan Dąbrowski <ivand195@outlook.com>
2018-12-16 19:42:52 +0100

    Consensus trees

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Bio import Phylo
+from Bio.Phylo.Consensus import *
 import tree_utils
 import click
 
@@ -9,8 +10,8 @@ def cli():
 
 
 @cli.command()
-@click.option('--file-path', help='File with phylo trees to parse')
-@click.option('--file-format', help='Phylo tree file format', default='newick')
+@click.option('-i', '--file-path', help='File with phylo trees to parse')
+@click.option('-f', '--file-format', help='Phylo tree file format', default='newick')
 @click.option('--draw/--no-draw', default=False, help='If used, tree will be drawn')
 @click.option('--ascii/--no-ascii', default=False, help='If used, tree will be drawn- ascii format')
 @click.option('--info/--no-info', default=False, help='If used, tree info will be shown')
@@ -32,6 +33,29 @@ def display(file_path, file_format, draw, ascii, info):
                 click.echo(tree)
     else:
         click.echo('Invalid or not existing file')
+
+
+@cli.command()
+@click.option('-i', '--file-path', help='File with phylo trees to parse')
+@click.option('-f', '--file-format', help='Phylo tree file format', default='newick')
+@click.option('-a', '--algorithm', help='Algorithm used for generating consensus tree', default='strict')
+@click.option('-m', '--majority-cutoff', default=0.5, help='Cutoff value for majority consensus algorithm', type=float)
+@click.option('-o', '--output-file', help='Output file path - generated tree will be saved to this file')
+def consensus(file_path, file_format, algorithm, majority_cutoff, output_file):
+    trees = tree_utils.load(file_path, file_format)
+    if algorithm.lower() == 'strict':
+        consensus_tree = strict_consensus(trees)
+    elif algorithm.lower() == 'majority':
+        consensus_tree = majority_consensus(trees, majority_cutoff)
+    elif algorithm.lower() == 'adam':
+        consensus_tree = adam_consensus(trees)
+    else:
+        click.echo('Invalid algorithm!')
+        return
+    click.echo('Drawing consensus tree...')
+    Phylo.draw(consensus_tree)
+    if output_file is not None:
+        Phylo.write(consensus_tree, output_file, file_format)
 
 
 if __name__ == '__main__':
