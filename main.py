953af6c11bf531dffa41426b5a8153bba7d09311
Jan Dąbrowski <ivand195@outlook.com>
2018-12-16 18:46:21 +0100

    Refactoring

--- main.py
+++ main.py
@@ -3,28 +3,36 @@ import tree_utils
 import click
 
 
-@click.command()
-@click.option('--file-path', help='File with phylo trees to parse')
-@click.option('--file-format', help='Phylo tree file format', default='NEWICK')
-@click.option('--draw/--no-draw', default=False, help='If used, tree will be drawn')
-@click.option('--ASCII/--no-ASCII', default=False, help='If used, tree will be drawn- ascii format')
-@click.option('--info/--no-info', default=False, help='If used, tree info will be shown')
+@click.group()
+def cli():
+    pass
 
+
+@cli.command()
+@click.option('--file-path', help='File with phylo trees to parse')
+@click.option('--file-format', help='Phylo tree file format', default='newick')
+@click.option('--draw/--no-draw', default=False, help='If used, tree will be drawn')
+@click.option('--ascii/--no-ascii', default=False, help='If used, tree will be drawn- ascii format')
+@click.option('--info/--no-info', default=False, help='If used, tree info will be shown')
 def display(file_path, file_format, draw, ascii, info):
     trees = tree_utils.load(file_path, file_format)
     if trees is not None:
         for tree in trees:
-            if draw:                
-                    Phylo.draw(tree)                
+            is_drawn = False
+            if draw:
+                Phylo.draw(tree)
+                is_drawn = True
             if ascii:
-                    tree_utils.drawASCII(tree)                
+                tree_utils.draw_ascii(tree)
+                is_drawn = True
             if info:
-                    tree_utils.showInfo(tree)               
-            else:
-                    click.echo(tree.name)
-                    click.echo(tree)
-        
+                tree_utils.show_info(tree)
+                is_drawn = True
+            if not is_drawn:
+                click.echo(tree)
+    else:
+        click.echo('Invalid or not existing file')
 
 
 if __name__ == '__main__':
-    display()
+    cli()
--- tree_utils.py
+++ tree_utils.py
@@ -13,39 +13,45 @@ def load(file_path, file_format):
         except:
             click.echo('File not found or invalid!')
             return None
-        
-def drawASCII(tree):
+
+
+def draw_ascii(tree):
     Phylo.draw_ascii(tree)
-    
-def getMaxDepth(depths):
-    max=0
+
+
+def get_max_depth(depths):
+    max = 0
     for node in depths:
         if not (node.name is None):
             curr = depths[node]
-            if (max< curr or min==0):
+            if max < curr or min == 0:
                 max = curr
-    return max       
-    
-def getMinDepth(depths):
-    min=0
+    return max
+
+
+def get_min_depth(depths):
+    min = 0
     for node in depths:
         if not (node.name is None):
             curr = depths[node]
-            if (min> curr or min==0):
+            if min > curr or min == 0:
                 min = curr
     return min
- 
-def showInfo(tree):
-    nonTerminalsCount = len(tree.get_terminals())
-    terminalCount = tree.count_terminals()
+
+
+def show_info(tree):
+    non_terminals_count = len(tree.get_terminals())
+    terminal_count = tree.count_terminals()
     depths = tree.depths()
-    print("Non terminals:",tree.get_nonterminals())
-    print("Non terminals count:",nonTerminalsCount)
-    print("Terminals:",tree.get_terminals())
-    print("Terminals count: ",terminalCount)
-    print("Total branch length: ",tree.total_branch_length())
-    print("Total node count:",nonTerminalsCount+terminalCount )
-    print("Is bifurcating: ",tree.is_bifurcating())
-    print("Is preterminal",tree.is_preterminal())
-    print("Max depth: ", getMaxDepth(depths))
-    print("Min depth: ", getMinDepth(depths))
+    if tree.name is not None:
+        print("Name: ", tree.name)
+    print("Non terminals: ", tree.get_nonterminals())
+    print("Non terminals count: ", non_terminals_count)
+    print("Terminals: ", tree.get_terminals())
+    print("Terminals count: ", terminal_count)
+    print("Total branch length: ", tree.total_branch_length())
+    print("Total node count: ", non_terminals_count + terminal_count)
+    print("Is bifurcating: ", tree.is_bifurcating())
+    print("Is preterminal: ", tree.is_preterminal())
+    print("Max depth: ", get_max_depth(depths))
+    print("Min depth: ", get_min_depth(depths))
