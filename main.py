fe7ce6401d3e1cef453b90ea899d79a412ad8f35
ewale <ewale@DESKTOP-D13LD70>
2018-12-16 18:05:19 +0100

    Display functions

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ import click
 @click.option('--file-path', help='File with phylo trees to parse')
 @click.option('--file-format', help='Phylo tree file format', default='NEWICK')
 @click.option('--draw/--no-draw', default=False, help='If used, tree will be drawn')
-def display(file_path, file_format, draw):
+@click.option('--ASCII/--no-ASCII', default=False, help='If used, tree will be drawn- ascii format')
+@click.option('--info/--no-info', default=False, help='If used, tree info will be shown')
+
+def display(file_path, file_format, draw, ascii, info):
     trees = tree_utils.load(file_path, file_format)
     if trees is not None:
-        if draw:
-            for tree in trees:
-                click.echo(tree.name)
-                click.echo(tree)
-        else:
-            for tree in trees:
-                tree.ladderize()
-                Phylo.draw(tree)
+        for tree in trees:
+            if draw:                
+                    Phylo.draw(tree)                
+            if ascii:
+                    tree_utils.drawASCII(tree)                
+            if info:
+                    tree_utils.showInfo(tree)               
+            else:
+                    click.echo(tree.name)
+                    click.echo(tree)
+        
 
 
 if __name__ == '__main__':
--- tree_utils.py
+++ tree_utils.py
@@ -13,3 +13,39 @@ def load(file_path, file_format):
         except:
             click.echo('File not found or invalid!')
             return None
+        
+def drawASCII(tree):
+    Phylo.draw_ascii(tree)
+    
+def getMaxDepth(depths):
+    max=0
+    for node in depths:
+        if not (node.name is None):
+            curr = depths[node]
+            if (max< curr or min==0):
+                max = curr
+    return max       
+    
+def getMinDepth(depths):
+    min=0
+    for node in depths:
+        if not (node.name is None):
+            curr = depths[node]
+            if (min> curr or min==0):
+                min = curr
+    return min
+ 
+def showInfo(tree):
+    nonTerminalsCount = len(tree.get_terminals())
+    terminalCount = tree.count_terminals()
+    depths = tree.depths()
+    print("Non terminals:",tree.get_nonterminals())
+    print("Non terminals count:",nonTerminalsCount)
+    print("Terminals:",tree.get_terminals())
+    print("Terminals count: ",terminalCount)
+    print("Total branch length: ",tree.total_branch_length())
+    print("Total node count:",nonTerminalsCount+terminalCount )
+    print("Is bifurcating: ",tree.is_bifurcating())
+    print("Is preterminal",tree.is_preterminal())
+    print("Max depth: ", getMaxDepth(depths))
+    print("Min depth: ", getMinDepth(depths))
